74e98172f1e597a1539339d8b1b179eb9741c628
g-birocci <gabrielbirocci@hotmail.com>
2025-10-28 10:58:39 +0000

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Vai da Bom caralhoooooo</h1>
+      <h1 className="bg-primary" >Vai da Bom caralhoooooo</h1>
       <h2>gfgrgergreger</h2>
     </>
   )
