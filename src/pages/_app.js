eb5233e7ad67e3d94f611bb77952e75bc3026aad
g-birocci <gabrielbirocci@hotmail.com>
2025-11-02 00:10:30 +0000

    correção _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,37 +1,30 @@
-<<<<<<< HEAD
-import Navbar from "@/components/Navbar";
-import Navmobile from "@/components/Navmobile";
-import Footer from "@/components/Footer";
-import "@/styles/globals.css";
-import "leaflet/dist/leaflet.css";
-=======
 import '@/styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import Head from 'next/head';
->>>>>>> main
+import Navbar from '@/components/layout/Navbar';
+import Navmobile from '@/components/Navmobile';
+import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-<<<<<<< HEAD
-      <div className="hidden md:block">
-        <Navbar />
-      </div>
-        <Component {...pageProps} />
-      <div className="md:hidden">
-        <Navmobile />
-      </div>
-        <Footer />
-=======
       <Head>
         <meta name="viewport" content="width=375, user-scalable=no, maximum-scale=1.0" />
       </Head>
+      <div className="hidden md:block">
+        <Navbar />
+      </div>
+      <div className="block md:hidden">
+        <Navmobile />
+      </div>
       <div className="app-outer">
         <div className="app-viewport">
           <Component {...pageProps} />
         </div>
       </div>
->>>>>>> main
+      <div>
+        <Footer/>
+      </div>
     </>
   );
 }
